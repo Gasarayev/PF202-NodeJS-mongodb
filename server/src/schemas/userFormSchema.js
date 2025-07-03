20888e694ed6603e30583411aa865ba8d257f16e
Gasarayev <gasarayev.emil@gmail.com>
2025-07-03 20:47:31 +0400

    feat:added schema for user form

---
+++ server/src/schemas/userFormSchema.js
@@ -0,0 +1,34 @@
+const mongoose = require("mongoose");
+
+const userFormSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  surname: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    lowercase: true,
+    trim: true,
+  },
+  profileImage: {
+    type: String, 
+    required: false,
+  },
+  message: {
+    type: String,
+    required: false,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+module.exports = userFormSchema;
